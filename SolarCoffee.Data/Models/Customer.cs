c48f9187083c411010fee8b54ad54f179cf3c5c1
Darkhan <darkhanahmetov228@gmail.com>
2021-02-03 12:50:47 +0600

    feat: added MakeFile, added first migration, added some models and changed Startup class

---
+++ SolarCoffee.Data/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SolarCoffee.Data.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public DateTime CreateOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        
+        public CustomerAddress PrimaryAddress { get; set; }
+    }
+}
